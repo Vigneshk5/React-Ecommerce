ecaa5b3113c2f266ff0c7a08211bbcd1c38cc84d
Vigneshk5 <vigneshpatali5@gmail.com>
2023-02-25 15:17:19 +0530

    footer

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,11 @@ const Home = ({ products, bannerData }) => {
         <p>speakers of different variation</p>
       </div>
       <div className="products-container">
-        {products?.map((product) => product.name)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
   );
 };
